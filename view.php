702a267a52d57312581704801efcd4a357c25d97
Chris Connelly <connec.2002@gmail.com>
2010-03-14 00:26:12 +0000

    Completely rewrote View to prepare for additional functionality.  Removed netbeans metadata.  Added todo file (notes_on_mvc.txt).

--- view.php
+++ view.php
@@ -1,279 +1,211 @@
 <?php
 
 /**
- * Contains the View class.
+ * Contains the View class and related exceptions.
  * 
- * @package utilities
+ * @package mvcwebcomponents
  * @author Chris Connelly
  */
+
 namespace MVCWebComponents;
-use MVCWebComponents\MVCException;
 
 /**
- * Generates a HTML page based on page templates, templated elements and assigned variables.
+ * Generates HTML code based on a template and given variables.
  * 
- * @version 0.3
+ * @version 0.4
  */
-Class View {
+class View
+{
 	
 	/**
-	 * A static register of views.
-	 * 
-	 * Format:
-	 * <code>
-	 * $views = array('ViewAlias' => View Instance);
-	 * </code>
-	 * 
-	 * @var array
-	 * @since 0.2
-	 */
-	private static $views = array();
-	
-	/**
-	 * A static register of elements.
-	 *
-	 * Format:
-	 * <code>
-	 * $elements = array('ElementAlias' => Element Instance);
-	 * </code>
-	 *
-	 * @var array
-	 * @since 0.2
-	 */
-	private static $elements = array();
-	
-	/**
-	 * The alias under which the view is stored.
+	 * A path to prepend to any given template paths for all templates.
 	 * 
 	 * @var string
-	 * @since 0.2
+	 * @since 0.4
 	 */
-	public $alias;
+	protected static $prePath = '';
 	
 	/**
-	 * The path to the template that the view instance will render.
+	 * A path/extension to append to any given template paths for all templates.
 	 * 
 	 * @var string
-	 * @since 0.2
+	 * @since 0.4
 	 */
-	public $template;
+	protected static $postPath = '';
 	
 	/**
-	 * Contains a register of all assigned template variables.
+	 * An associative array of variable => 'value' pairs to pass to the template.
 	 * 
-	 * Format:
-	 * <code>
-	 * $register = array('templateVariableName' => "templateVariableName's Value");
-	 * </code>
-	 *
 	 * @var array
-	 * @since 0.2
+	 * @since 0.4
 	 */
-	private $register = array();
+	protected $register = array();
 	
 	/**
-	 * Whether or not to return the result of the template instead of displaying it.
-	 *
+	 * The path to this Views template.
+	 * 
+	 * @var string
+	 * @since 0.4
+	 */
+	protected $template = '';
+	
+	/**
+	 * The result of the View's render.
+	 * 
+	 * @var string
+	 * @since 0.4
+	 */
+	protected $result = '';
+	
+	/**
+	 * Whether or not to return the render result.
+	 * 
 	 * @var bool
-	 * @since 0.2
+	 * @since 0.4
 	 */
-	public $return = false;
+	protected $return = false;
 	
 	/**
-	 * Stores the result of the template.
-	 *
-	 * @var string
-	 * @since 0.2
-	 */
-	public $result;
-	
-	/**
-	 * Overloads variable setting.
+	 * Set the prePath.
 	 * 
-	 * Registers variables assigned to the view as template variables unless they are predefined by the class.
-	 *
-	 * @param string $var The name of the variable being assigned.
-	 * @param string $value The value being assigned.
+	 * @param string $prePath
+	 * @since 0.4
+	 */
+	public static function setPrePath($prePath) {
+		
+		static::$prePath = $prePath;
+		
+	}
+	
+	/**
+	 * Set the postPath.
+	 * 
+	 * @param string $postPath
+	 * @since 0.4
+	 */
+	public static function setPostPath($postPath) {
+		
+		static::$postPath = $postPath;
+		
+	}
+	
+	/**
+	 * Get a new View object for a given template.
+	 * 
+	 * @param string $template The path to the template to render.
 	 * @return void
-	 * @since 0.2
+	 * @since 0.4
 	 */
-	public function __set($var, $value) {
+	public function __construct($template) {
 		
-		if(!in_array($var, get_class_vars(get_class($this)))) {
-			$this->register[$var] = $value;
-		}else {
-			$this->{$var} = $value;
-		}
+		$this->template = static::$prePath . $template . static::$postPath;
 		
 	}
 	
 	/**
-	 * Allows returning of template variables assigned to {@link $register}.
-	 *
-	 * @param string $var The name of the variable to return.
-	 * @return mixed The value of $var.
-	 * @since 0.2
-	 */
-	public function __get($var) {
-		
-		if(!in_array($var, get_class_vars(get_class($this)))) {
-			return $this->register[$var];
-		}else {
-			return $this->{$var};
-		}
-		
-	}
-	
-	/**
-	 * Get an instance of a view.
+	 * Shortcut to assign template variables.
 	 * 
-	 * Searches the {@link $views} variable for the view referenced by $viewAlias and returns it if it exists, or creates and returns it if not.
-	 * 
-	 * @param string $viewAlias The alias (or path) of the view to return.
-	 * @return object An instance of the view referenced by $viewAlias.
-	 * @since 0.2
-	 */
-	public static function &getView($viewAlias) {
-		
-		if(isset(View::$views[$viewAlias])) {
-			return View::$views[$viewAlias];
-		}else {
-			View::$views[$viewAlias] = new View($viewAlias);
-			return View::$views[$viewAlias];
-		}
-		
-	}
-	
-	/**
-	 * Get an instance of an element.
-	 *
-	 * Searches the {@link $elements} variable for the element referenced by $elementAlias and returns it if it exists, or creates and returns it if not.
-	 *
-	 * @param string $elementAlias The alias (or path) of the element to return.
-	 * @return object An instance of the element referred to by $elementAlias.
-	 * @since 0.2
-	 */
-	public static function &getElement($elementAlias) {
-		
-		if(isset(View::$elements[$elementAlias])) {
-			return View::$elements[$elementAlias];
-		}else {
-			View::$elements[$elementAlias] = new View($elementAlias);
-			View::$elements[$elementAlias]->return = true;
-			return View::$elements[$elementAlias];
-		}
-		
-	}
-	
-	/**
-	 * Assigns an alias and default view path to the newly created view.
-	 * 
-	 * @param string $viewAlias The alias and default view path for the view.
+	 * @param string $key   The key being assigned to.
+	 * @param mixed  $value The value to assign.
 	 * @return void
-	 * @since 0.2
+	 * @since 0.4
 	 */
-	public function __construct($viewAlias) {
+	public function __set($key, $value) {
 		
-		// Assign default values for alias and template.
-		$this->alias = $viewAlias;
-		$this->template = $viewAlias . '.tpl';
+		$this->register[$key] = $value;
 		
 	}
 	
 	/**
-	 * Renders the view to the browser, unless {@link $return} is set to true.
-	 *
-	 * @return mixed Void, unless {@link $return} is true, in which case returns the rendered page.
-	 * @since 0.2
+	 * Getter for {@link $result}.
+	 * 
+	 * @return string
+	 * @since 0.4
 	 */
-	public function render() {
+	public function getResult() {
+		return $this->result;
+	}
+	
+	/**
+	 * Register a variable to pass to the template.
+	 * 
+	 * @param string $key   The name to use to represent the variable.
+	 * @param mixed  $value The value to store.
+	 * @return void
+	 * @since 0.4
+	 */
+	public function register($key, $value) {
 		
-		// Check the template exists.
-		if(!$this->templateExists()) {
-			$this->result = "Template '$this->template' not found.";
-			throw new MissingTemplateException($this->template);
-		}else {
-			// Load the registered template variables into the local scope.
-			extract($this->register);
-			
-			// Start output buffering.
-			ob_start();
-			
-			// Load the template.
-			include $this->template;
-			
-			// Get the result of the template from the output buffer.
-			$this->result = ob_get_clean();
-		}
+		$this->register[$key] = $value;
 		
+	}
+	
+	/**
+	 * Render the given template.
+	 * 
+	 * @param bool $return When true the result is returned instead of echo'd.
+	 * @return mixed String if $return = true and the template is available, void otherwise.
+	 * @since 0.4
+	 */
+	public function render($return = false) {
+		
+		if(!checkTemplate()) return;
+		
+		$this->return = (bool)$return; // Store $return in an instance variable for improved sandboxing.
+		
+		// Extract the register into the local scope.
+		extract($this->register);
+		
+		// Begin output buffering.
+		ob_start();
+		
+		// Include the template.
+		include $this->template;
+		
+		// Store the result.
+		$this->result = ob_get_clean();
+		
+		// Return or display it.
 		if($this->return) return $this->result;
-		else echo $this->result;
+		echo $this->result;
 		
 	}
 	
 	/**
-	 * Checks if {@link $template} exists, returns true if so and false otherwise.
-	 *
-	 * @return bool True if {@link $template} exists, false otherwise.
-	 * @since 0.2
+	 * Checks the view's template exists, and throws an exception otherwise.
+	 * 
+	 * @return bool True if the template exists and is readable, false otherwise.
+	 * @throws {@link MissingTemplateException} Thrown when the template cannot be found/read.
+	 * @since 0.4
 	 */
-	public function templateExists() {
+	public function checkTemplate() {
 		
-		if(file_exists($this->template)) return true;
-		else return false;
-		
-	}
-	
-	/**
-	 * Links a helper to the view for use by templates.
-	 *
-	 * @param string $helper The path to the helper.
-	 * @return bool True if the helper was found and loaded false otherwise.
-	 * @since 0.2
-	 */
-	public function useHelper($helper) {
-		
-		$helper = "$helper.php";
-		if(!file_exists($helper)) return false;
-		
-		$helperName = Inflector::camelize(basename($helper, '.php'));
-		
-		// Include the $helper file.
-		include_once $helper;
-		
-		// Check the class was loaded.
-		if(!class_exists($helperName)) return false;
-		
-		// Register it with the... register.
-		$this->{$helperName} = new $helperName;
-		
-		return true;
+		if(is_readable($this->template)) return true;
+		throw new MissingTemplateException($this->template);
+		return false;
 		
 	}
 	
 }
 
 /**
- * Missing template exception.
- *
- * An exception thrown when View->render() encounters a missing template.
- *
- * @subpackage exceptions
+ * Thrown when {@link View::checkTemplate()} encounters an unreadable template.
+ * 
+ * @package mvcwebcomponents.exceptions
  * @version 1.0
  */
-
-Class MissingTemplateException extends MVCException {
+class MissingTemplateException extends MVCException {
 	
 	/**
-	 * Assigns the message to the exception.
-	 *
-	 * @param string $template The missing template.
+	 * Assign the message based on the missing template.
+	 * 
+	 * @param string $template The path of the missing template.
 	 * @return void
 	 * @since 1.0
 	 */
-	public function MissingTemplateException($template) {
+	public function __construct($template) {
 		
-		$this->message = "Could not find template '$template'.";
+		$this->message = "Can not read template `$template` for view.  Ensure it exists and is readable.";
 		
 	}
 	
